14ab0d527544591f469d3bddb7e04e5a6286caef
Piotr-reader <piotr_partsianko@inbox.ru>
2022-05-24 01:51:59 +0300

    feat:implement localstorage function

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+let arrNumber = [];
+let arrAnswer = [];
 const allInput = document.querySelectorAll(".form__field");
 const allBtn = document.querySelectorAll(".form__button");
+const totalQuestions = allInput.length;
+document.querySelector(`.total_questions`).innerHTML = totalQuestions;
+
 allBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     allInput.forEach((input) => {
       if (btn.classList.contains(input.id)) {
         let numberPuzzle = input.id;
-        let inputVal = input.value.toLowerCase();
+        let inputVal = input.value;
         if (btn.classList.contains("form__button_type_submit")) {
-          console.log(numberPuzzle);
-          if (inputVal === input.dataset.answer) {
+          if (inputVal.toLowerCase() === input.dataset.answer) {
             document.querySelector(`.correct-answer-board.${numberPuzzle}`).style.display = "block";
             document.querySelector(`.wrong-answer-board.${numberPuzzle}`).style.display = "none";
             document.querySelector(`.hint-board.${numberPuzzle}`).style.display = "none";
+            // btn.setAttribute("disabled", "disabled");
+            arrNumber.push(numberPuzzle);
+            arrNumber = [...new Set(arrNumber)];
+            localStorage.setItem('numberPuzzle', JSON.stringify(arrNumber));
+            arrAnswer.push(inputVal);
+            console.log(arrAnswer);
+            arrAnswer = [...new Set(arrAnswer)];
+            localStorage.setItem('answers', JSON.stringify(arrAnswer));
+            let correctAnswers = arrAnswer.length;
+            document.querySelector(`.correct_answers`).innerHTML = correctAnswers;
+            document.querySelector(`.quolity`).innerHTML = Math.floor((correctAnswers/totalQuestions)*100);
           } else {
             document.querySelector(`.wrong-answer-board.${numberPuzzle}`).style.display = "block";
             document.querySelector(`.correct-answer-board.${numberPuzzle}`).style.display = "none";
@@ -26,3 +41,23 @@ allBtn.forEach((btn) => {
     });
   });
 });
+
+let localstorageAnswers = localStorage.getItem('answers');
+let localstoragenumberPuzzle = localStorage.getItem('numberPuzzle');
+if (localstorageAnswers !== null) {
+  answers = JSON.parse(localstorageAnswers);
+  numberPuzzle = JSON.parse(localstoragenumberPuzzle);
+    for (let i = 0; i < answers.length; i++) {
+      document.getElementById(`${numberPuzzle[i]}`).value = answers[i];
+      document.querySelector(`.correct-answer-board.${numberPuzzle[i]}`).style.display = "block";
+      arrNumber.push(numberPuzzle[i]);
+      arrNumber = [...new Set(arrNumber)];
+      arrAnswer.push(answers[i]);
+      arrAnswer = [...new Set(arrAnswer)];
+    }
+    let correctAnswers = arrAnswer.length;
+    document.querySelector(`.correct_answers`).innerHTML = correctAnswers;
+    document.querySelector(`.quolity`).innerHTML = Math.floor((correctAnswers/totalQuestions)*100);
+  }
+
+
